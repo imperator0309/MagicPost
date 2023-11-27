444b10a4b65b10d42b7095477f4b7ea2d5171a11
imperator0309 <respvblicaromana0309@gmail.com>
2023-11-28 00:28:02 +0700

    Add editing and deleting functions for director

--- src/backend/controllers/DirectorController.js
+++ src/backend/controllers/DirectorController.js
@@ -4,9 +4,22 @@ const Bases = require('../models/Bases')
 const jwt = require('jsonwebtoken')
 const {multipleMongooseToObject, mongooseToObject} = require('../../backend/ulti/mongoose')
 const {addSecret, removeSecret, getSecret} = require('../ulti/mappingSecret')
+const { json } = require('express')
 
 class DirectorController {
-    //[POST] /director/create-account
+
+    //[GET] /director/accounts
+    showAccounts(req, res, next) {
+        Actors.find({role: [1, 2]})
+            .then(actors => {
+                res.json({
+                    querySuccess: 'true',
+                    accounts: multipleMongooseToObject(actors)
+                })
+            })
+    }
+
+    //[POST] /director/accounts/create
     createAccount(req, res, next) {
         if (req.cookies.id) {
             var userID = jwt.verify(req.cookies.id, 'secret').userID
@@ -42,7 +55,131 @@ class DirectorController {
         }
     }
 
-    //[POST] /director/create-base
+    //[GET] /director/accounts/edit/:accountID
+    editAccount(req, res, next) {
+        if (req.cookies.id) {
+            var userID = jwt.verify(req.cookies.id, 'secret').userID
+            Actors.findById(userID)
+                .then(director => {
+                    var querySuccess = false
+                    if (director) {
+                        if (director.role == 0) {
+                            querySuccess = true
+                        }
+                    }
+
+                    if (querySuccess) {
+                        Actors.findById(req.params.accountID)
+                            .then(actor => {
+                                actor = mongooseToObject(actor)
+                                actor.querySuccess = 'true'
+                                res.json({
+                                    querySuccess: 'true',
+                                    account: actor
+                                })
+                            })
+                    } else {
+                        res.json({querySuccess: 'false', message: 'Permission Denied'})
+                    }
+                })
+        } else {
+            res.json({querySuccess: 'false', message: 'Permission Denied'})
+        }
+    }
+
+    //[PUT] /director/accounts/edit/:id
+    updateAccount(req, res, next) {
+        if (req.cookies.id) {
+            var userID = jwt.verify(req.cookies.id, 'secret').userID
+            Actors.findById(userID)
+                .then(director => {
+                    var querySuccess = false
+                    if (director) {
+                        if (director.role == 0) {
+                            querySuccess = true
+                        }
+                    }
+
+                    if (querySuccess) {
+                        Actors.updateOne({_id: req.params.accountID}, req.body)
+                            .then(() => {
+                                res.json({querySuccess: 'true', message: 'Updated account successfully'})
+                            })
+                    } else {
+                        res.json({querySuccess: 'false', message: 'Permission Denied'})
+                    }
+                })
+        } else {
+            res.json({querySuccess: 'false', message: 'Permission Denied'})
+        }
+    }
+
+    //[DELETE] /director/accounts/delete
+    deleteAccount(req, res, next) {
+        if (req.cookies.id) {
+            var userID = jwt.verify(req.cookies.id, 'secret').userID
+            Actors.findById(userID)
+                .then(director => {
+                    var querySuccess = false
+                    if (director) {
+                        if (director.role == 0) {
+                            querySuccess = true
+                        }
+                    }
+
+                    if (querySuccess) {
+                        Actors.deleteOne({_id: req.body.accountID})
+                            .then(() => {
+                                res.json({querySuccess: 'true', message: 'Delete Account Successfully'})
+                            })
+                    } else {
+                        res.json({querySuccess: 'false', message: 'Permission Denied'})
+                    }
+                })
+        } else {
+            res.json({querySuccess: 'false', message: 'Permission Denied'})
+        }
+    }
+
+    //[DELETE] /director/accounts/multiple-delete
+    deleteMultipleAccounts(req, res, next) {
+        if (req.cookies.id) {
+            var userID = jwt.verify(req.cookies.id, 'secret').userID
+            Actors.findById(userID)
+                .then(director => {
+                    var querySuccess = false
+                    if (director) {
+                        if (director.role == 0) {
+                            querySuccess = true
+                        }
+                    }
+
+                    if (querySuccess) {
+                        Actors.deleteMany({_id: {$in: req.body.accountIDs}})
+                            .then(() => {
+                                res.json({querySuccess: 'true', message: 'Delete Accounts Successfully'})
+                            })
+                    } else {
+                        res.json({querySuccess: 'false', message: 'Permission Denied'})
+                    }
+                })
+        } else {
+            res.json({querySuccess: 'false', message: 'Permission Denied'})
+        }
+    }
+
+    //[GET] /director/bases
+    showBases(req, res, next) {
+        Bases.find({})
+            .then(bases => {
+                res.json({
+                    querySuccess: 'true',
+                    bases: multipleMongooseToObject(bases)
+                })
+            })
+    }
+
+    //[POST] /director/bases/create
     createBase(req, res, next) {
         if (req.cookies.id) {
             var userID = jwt.verify(req.cookies.id, 'secret').userID
@@ -78,6 +215,120 @@ class DirectorController {
             res.json({querySuccess: 'false', message: 'Permission Denied'})
         }
     }
+
+    //[GET] /director/bases/edit/:id
+    editBase(req, res, next) {
+        if (req.cookies.id) {
+            var userID = jwt.verify(req.cookies.id, 'secret').userID
+            Actors.findById(userID)
+                .then(director => {
+                    var querySuccess = false
+                    if (director) {
+                        if (director.role == 0) {
+                            querySuccess = true
+                        }
+                    }
+
+                    if (querySuccess) {
+                        Bases.findById(req.params.baseID)
+                            .then(base => {
+                                base = mongooseToObject(base)
+                                base.querySuccess = 'true'
+                                res.json({
+                                    querySuccess: 'true',
+                                    base: base
+                                })
+                            })
+                    } else {
+                        res.json({querySuccess: 'false', message: 'Permission Denied'})
+                    }
+                })
+        } else {
+            res.json({querySuccess: 'false', message: 'Permission Denied'})
+        }
+    }
+
+    //[PUT] /director/bases/edit/:id
+    updateBase(req, res, next) {
+        if (req.cookies.id) {
+            var userID = jwt.verify(req.cookies.id, 'secret').userID
+            Actors.findById(userID)
+                .then(director => {
+                    var querySuccess = false
+                    if (director) {
+                        if (director.role == 0) {
+                            querySuccess = true
+                        }
+                    }
+
+                    if (querySuccess) {
+                        Bases.updateOne({_id: req.params.baseID}, req.body)
+                            .then(() => {
+                                res.json({querySuccess: 'true', message: 'Updated Base Successfully'})
+                            })
+                    } else {
+                        res.json({querySuccess: 'false', message: 'Permission Denied'})
+                    }
+                })
+        } else {
+            res.json({querySuccess: 'false', message: 'Permission Denied'})
+        }
+    }
+
+    //[DELETE] /director/bases/delete
+    deleteBase(req, res, next) {
+        if (req.cookies.id) {
+            var userID = jwt.verify(req.cookies.id, 'secret').userID
+            Actors.findById(userID)
+                .then(director => {
+                    var querySuccess = false
+                    if (director) {
+                        if (director.role == 0) {
+                            querySuccess = true
+                        }
+                    }
+
+                    if (querySuccess) {
+                        Bases.deleteOne({_id: req.body.baseID})
+                            .then(() => {
+                                res.json({querySuccess: 'true', message: 'Delete Base Successfully'})
+                            })
+                    } else {
+                        res.json({querySuccess: 'false', message: 'Permission Denied'})
+                    }
+                })
+        } else {
+            res.json({querySuccess: 'false', message: 'Permission Denied'})
+        }
+    }
+
+    //[DELETE] /director/bases/multiple-delete
+    deleteMultipleBases(req, res, next) {
+        if (req.cookies.id) {
+            var userID = jwt.verify(req.cookies.id, 'secret').userID
+            Actors.findById(userID)
+                .then(director => {
+                    var querySuccess = false
+                    if (director) {
+                        if (director.role == 0) {
+                            querySuccess = true
+                        }
+                    }
+
+                    if (querySuccess) {
+                        Actors.deleteMany({_id: {$in: req.body.baseIDs}})
+                            .then(() => {
+                                res.json({querySuccess: 'true', message: 'Delete Bases Successfully'})
+                            })
+                    } else {
+                        res.json({querySuccess: 'false', message: 'Permission Denied'})
+                    }
+                })
+        } else {
+            res.json({querySuccess: 'false', message: 'Permission Denied'})
+        }
+    }
 }
+
 
 module.exports = new DirectorController
--- src/backend/routes/director.js
+++ src/backend/routes/director.js
@@ -3,7 +3,14 @@ const router = express.Router()
 
 const DirectorController = require('../controllers/DirectorController')
 
-router.post('/create-account', DirectorController.createAccount)
-router.post('/create-base', DirectorController.createBase)
-
+router.post('/accounts/create', DirectorController.createAccount)
+router.post('/bases/create', DirectorController.createBase)
+router.delete('/accounts/delete', DirectorController.deleteAccount)
+router.delete('/accounts/multiple-delete', DirectorController.deleteMultipleAccounts)
+router.delete('/bases/delete', DirectorController.deleteBase)
+router.delete('/bases/multiple-delete', DirectorController.deleteMultipleBases)
+router.get('/accounts/edit/:id', DirectorController.editAccount)
+router.get('/bases/edit/:id', DirectorController.editBase)
+router.get('/accounts', DirectorController.showAccounts)
+router.get('/bases', DirectorController.showBases)
 module.exports = router
